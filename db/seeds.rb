be45cadd7f5036e36714832d72ffa7c5fbeef5f6
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-14 12:41:04 +0800

    Cocktail Working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require 'json'
 require 'open-uri'
 
+puts 'Destroying the ingredients..'
+Ingredient.destroy_all if Rails.env.development?
+puts 'Destroying the cocktails..'
+Cocktail.destroy_all if Rails.env.development?
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
@@ -11,4 +16,4 @@ ingredients['drinks'].each do |ingredient|
   element = Ingredient.create(name: ingredient['strIngredient1'])
 end
 
-puts 'Seeds created!'
+puts 'Ingredients created!'
